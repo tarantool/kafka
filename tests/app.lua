383524fd5261cac908bb76139f9eec26e42f6f2d
Michael Filonenko <filonenko.mikhail@gmail.com>
2023-02-08 10:51:57 +0300

    adsfasfd

--- tests/app.lua
+++ tests/app.lua
@@ -10,6 +10,8 @@ box.once('init', function()
     box.schema.user.grant("guest", 'read,write,execute,create,drop', 'universe')
 end)
 
+local cons, err = pcall(require, 'tests.consumer')
+require('log').info(err)
 
 rawset(_G, 'consumer', require('tests.consumer'))
 rawset(_G, 'producer', require('tests.producer'))
