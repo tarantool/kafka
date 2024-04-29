677ad66ce3ba41a70157ada94f59273a7de80386
Oleg Babin <babinoleg@mail.ru>
2024-04-29 15:57:16 +0300

    fix memory leak in case of error on consumer/producer creation
    Before this patch we didn't free some resources in case of error
    in Consumer.create or Producer.create functions. This patch fixes
    it.
    
    Closes #108

--- kafka/producer.h
+++ kafka/producer.h
@@ -5,44 +5,6 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-#include <librdkafka/rdkafka.h>
-
-#include <queue.h>
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
- * Producer
- */
-
-typedef struct {
-    rd_kafka_t      *rd_producer;
-    pthread_t       thread;
-    pthread_attr_t  attr;
-    int             should_stop;
-    pthread_mutex_t lock;
-} producer_poller_t;
-
-typedef struct {
-    rd_kafka_topic_t **elements;
-    int32_t count;
-    int32_t capacity;
-} producer_topics_t;
-
-producer_topics_t *new_producer_topics(int32_t capacity);
-
-void
-add_producer_topics(producer_topics_t *topics, rd_kafka_topic_t *element);
-
-void
-destroy_producer_topics(producer_topics_t *topics);
-
-typedef struct {
-    rd_kafka_t        *rd_producer;
-    producer_topics_t *topics;
-    event_queues_t    *event_queues;
-    producer_poller_t *poller;
-} producer_t;
-
 int
 lua_producer_tostring(struct lua_State *L);
 
